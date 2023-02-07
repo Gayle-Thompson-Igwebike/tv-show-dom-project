582dea0dc483e79fb1ca78f096bb1ac6c24143a0
Gayle Ngozi <gaylengozi@gmail.com>
2023-02-07 20:19:11 +0000

    resolving issues on level 300...

--- script.js
+++ script.js
@@ -2,16 +2,16 @@
 
 //Making this variable global in order to use again//
 const allEpisodes = getAllEpisodes();
+const rootElem = document.getElementById("root");
 
 function setup() {
   makePageForEpisodes(allEpisodes);
   episodeSelector(allEpisodes);
-  searchItem(allEpisodes);
+  searchItem();
 }
 
 //LEVEL 100 START//
 function makePageForEpisodes() {
-  const rootElem = document.getElementById("root");
   rootElem.innerHTML = "";
   const allEpisodesDisplay = document.getElementById("all-Episodes");
   allEpisodesDisplay.textContent = `Got ${allEpisodes.length} episode(s)`;
@@ -55,6 +55,10 @@ function makePageForEpisodes() {
   }
 }
 
+// Create a div and append all:labe, select, input, button
+// let headingTags = document.getElementsByClassName("headers");
+
+
 //LEVEL 200 START//
 //Start a function for searching for episodes//
 function searchItem() {
@@ -73,31 +77,65 @@ function searchItem() {
   });
 }
 
-//LEVEL 300 START//
-//Start a function for selecting episodes//
-function episodeSelector() {
-  const episodeSelector = document.getElementById("episodes-selector");
-  for (let episode of getAllEpisodes()) {
-    const option = document.createElement("option");
-    episodeSelector.appendChild(option);
-    option.value = episode.name;
-    const episodeSeason = episode.season;
-    const episodeNumber = episode.number;
-    const codeDiv = `S${String(episodeSeason).padStart(2, "0")}E${String(
-      episodeNumber
-    ).padStart(2, "0")}`;
-    option.innerText = `${codeDiv} - ${episode.name}`;
+// //LEVEL 300 START//
+function episodeSelector(episodeList) {
+  const episodSelect = document.getElementById("episodes-selector");
+  for (let episode of episodeList) {
+    const episodeOption = document.createElement("option");
+    episodeOption.innerText = `S${String(episode.season).padStart(
+      2,
+      "0"
+    )}E${String(episode.number).padStart(2, "0")} ${episode.name}`;
+    episodSelect.appendChild(episodeOption);
   }
-  //Add eventlistener//
-  episodeSelector.addEventListener("change", (event) => {
-    const selectAllEpisodes = event.target.value;
-    const allEpisodes = getAllEpisodes();
-    const showAll = allEpisodes.find((element) =>
-      element.name.includes(selectAllEpisodes)
-    );
-    makePageForEpisodes(showAll);
-    console.log(showAll);
+  episodSelect.addEventListener("change", (event) => {
+    const keyValues = event.target.value;
+    const myTitle = keyValues.slice(7);
+    rootElem.innerHTML = "";
+    if (keyValues === "Show All Episodes") {
+      makePageForEpisodes(episodeList);
+    } else {
+      const episodeFilter = episodeList.filter((searchedEpisodes) => {
+        return searchedEpisodes.name.includes(myTitle);
+      });
+      makePageForEpisodes(episodeFilter);
+    }
   });
 }
 
+
+
+
+
+
+
+// //Start a function for selecting episodes//
+// function episodeSelector() {
+//   const episodeSelector = document.getElementById("episodes-selector");
+//   for (let episode of getAllEpisodes()) {
+//     const option = document.createElement("option");
+//     episodeSelector.appendChild(option);
+//     option.value = episode.name;
+//     const episodeSeason = episode.season;
+//     const episodeNumber = episode.number;
+//     const codeDiv = `S${String(episodeSeason).padStart(2, "0")}E${String(
+//       episodeNumber
+//     ).padStart(2, "0")}`;
+//     option.innerText = `${codeDiv} - ${episode.name}`;
+//   }
+//   //Add eventlistener//
+//   episodeSelector.addEventListener("change", (event) => {
+//     const selectAllEpisodes = event.target.value;
+//     // const allEpisodes = getAllEpisodes();
+//     const showAll = allEpisodes.find((element) =>
+//       element.name.includes(selectAllEpisodes)
+//     );
+//     makePageForEpisodes(showAll);
+//     console.log(showAll);  
+//   });
+
+// }
+
 window.onload = setup;
+
+
